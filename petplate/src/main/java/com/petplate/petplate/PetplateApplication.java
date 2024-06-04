611c6c667c082adce45ad603e29375ff16343b14
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-06-04 14:49:26 +0900

    Setting: initial commit

---
+++ petplate/src/main/java/com/petplate/petplate/PetplateApplication.java
@@ -0,0 +1,13 @@
+package com.petplate.petplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetplateApplication.class, args);
+	}
+
+}
---
+++ petplate/src/test/java/com/petplate/petplate/PetplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petplate.petplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
