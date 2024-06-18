45a794b06d0c988dc310bb789134a2426f1e8131
Jinu Yi <dionisos198@naver.com>
2024-06-19 03:29:38 +0900

    Test: EnumType 테스트 (#5)

--- src/test/java/com/petplate/petplate/PetplateApplicationTests.java
+++ src/test/java/com/petplate/petplate/PetplateApplicationTests.java
@@ -1,5 +1,8 @@
 package com.petplate.petplate;
 
+import com.petplate.petplate.common.EmbeddedType.Nutrient;
+import com.petplate.petplate.common.EmbeddedType.StandardNutrient;
+import com.petplate.petplate.common.EmbeddedType.Vitamin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,26 @@ class PetplateApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		Nutrient nutrient=Nutrient.builder()
+				.carbonHydrate((float) 24)
+				.fat((float) 100)
+				.vitamin(Vitamin.builder().vitaminD((float) 3.6).vitaminA((float) 3.5).vitaminB( (float) 3.2).vitaminE((float)2.1).build())
+				.phosphorus((float) 1.4)
+				.calcium((float)9.8)
+				.protein((float) 50)
+				.build();
+
+		//가장 부족한 영양소 이름
+		StandardNutrient deficientNutrient = StandardNutrient.findDeficientNutrient(nutrient);
+		System.out.println(deficientNutrient.getName());
+
+		//가장 많은 영양소 이름
+		StandardNutrient sufficientNutrient = StandardNutrient.findSufficientNutrient(nutrient);
+		System.out.println(sufficientNutrient.getName());
+
+
+
 	}
 
 }
