4d6fc885167e04e0a1fae21c288edebd9c4c7933
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-06-20 18:03:22 +0900

    Feat: 반려견 추가 (#10)

---
+++ src/main/java/com/petplate/petplate/pet/domain/dto/request/AddPetRequestDto.java
@@ -0,0 +1,31 @@
+package com.petplate.petplate.pet.domain.dto.request;
+
+import com.petplate.petplate.pet.domain.Activity;
+import com.petplate.petplate.user.domain.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AddPetRequestDto {
+    @NotBlank(message = "잘못된 이름 입력입니다. (이름은 최소 한글자 이상 입력되어야 합니다.)")
+    private String name;
+    @NotNull(message = "나이가 입력되지 않았습니다.")
+    @Min(value = 0, message = "잘못된 나이 입력입니다. (나이는 최소 1살부터 최대 40살까지입니다.)")
+    @Max(value = 41, message = "잘못된 나이 입력입니다. (나이는 최소 1살부터 최대 40살까지입니다.)")
+    private int age;
+    @NotNull(message = "체중이 입력되지 않았습니다.")
+    @Min(value = 0, message = "잘못된 체중 입력입니다. (체중은 0kg 초과 100kg 미만입니다.)")
+    @Max(value = 100, message = "잘못된 체중 입력입니다. (체중은 0kg 초과 100kg 미만입니다.)")
+    private double weight;
+    @NotNull(message = "활동량이 입력되지 않았습니다.")
+    private Activity activity;
+    @NotNull(message = "중성화 여부가 입력되지 않았습니다.")
+    private boolean isNeutering;
+}
