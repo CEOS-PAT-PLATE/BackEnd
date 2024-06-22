b0c228f42fc1e09890142b8dd7c81c9854af328a
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-06-22 23:33:39 +0900

    Feat: 영양소 이름으로 영양소 양 조회하는 기능 (#17)

--- src/main/java/com/petplate/petplate/common/EmbeddedType/Nutrient.java
+++ src/main/java/com/petplate/petplate/common/EmbeddedType/Nutrient.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,12 +33,36 @@ public class Nutrient {
 
     @Builder
     public Nutrient(double carbonHydrate, double protein, double fat, double calcium, double phosphorus,
-            Vitamin vitamin) {
+                    Vitamin vitamin) {
         this.carbonHydrate = carbonHydrate;
         this.protein = protein;
         this.fat = fat;
         this.calcium = calcium;
         this.phosphorus = phosphorus;
         this.vitamin = vitamin;
+    }
+
+    public double getNutrientWeightByName(String name) {
+        return switch (name) {
+            case "carbonHydrate" -> carbonHydrate;
+            case "탄수화물" -> carbonHydrate;
+            case "protein" -> protein;
+            case "단백질" -> protein;
+            case "fat" -> fat;
+            case "지방" -> fat;
+            case "calcium" -> calcium;
+            case "칼슘" -> calcium;
+            case "phosphorus" -> phosphorus;
+            case "인" -> phosphorus;
+            case "vitaminA" -> vitamin.getVitaminA();
+            case "비타민A" -> vitamin.getVitaminA();
+            case "vitaminB" -> vitamin.getVitaminB();
+            case "비타민B" -> vitamin.getVitaminB();
+            case "vitaminD" -> vitamin.getVitaminD();
+            case "비타민D" -> vitamin.getVitaminD();
+            case "vitaminE" -> vitamin.getVitaminE();
+            case "비타민E" -> vitamin.getVitaminE();
+            default -> throw new RuntimeException("Unknown nutrient name: " + name);
+        };
     }
 }
