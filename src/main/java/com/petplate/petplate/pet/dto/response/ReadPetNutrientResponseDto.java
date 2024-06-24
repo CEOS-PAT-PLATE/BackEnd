32dedf4e687e73cb7cb30f41e1191fe806cbcc22
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-06-25 00:29:08 +0900

    Refactor: 반환 정보 추가 (#24)
    최소 섭취량, 최대 섭취량, 최소 섭취량 대비 섭취량 비율, 최소 섭취량 대비 최대 섭취량 비율도 추가 반환

--- src/main/java/com/petplate/petplate/pet/dto/response/ReadPetNutrientResponseDto.java
+++ src/main/java/com/petplate/petplate/pet/dto/response/ReadPetNutrientResponseDto.java
@@ -4,17 +4,25 @@ import lombok.Getter;
 
 @Getter
 public class ReadPetNutrientResponseDto {
-    private String name;
-    private String unit;
-    private String description;
-    private double amount;
+    private String name;  // 이름
+    private String unit;  // 영양소 단위
+    private String description;  // 영양소 설명
+    private double amount;  // 섭취량
+    private double minimumIntake; // 최소 섭취량
+    private double maximumIntake; // 최대 섭취량
+    private double amountRatioPerMinimumIntake;  // 최소 섭취량 대비 섭취량 비율
+    private double maximumIntakeRatioPerMinimumIntake;  // 최소 섭취량 대비 최대 섭취량 비율
 
-    public static ReadPetNutrientResponseDto from(String name, String unit, String description, double amount) {
+    public static ReadPetNutrientResponseDto of(String name, String unit, String description, double amount, double minimumIntake, double maximumIntake) {
         ReadPetNutrientResponseDto response = new ReadPetNutrientResponseDto();
         response.name = name;
         response.unit = unit;
         response.description = description;
         response.amount = amount;
+        response.minimumIntake = minimumIntake;
+        response.maximumIntake = maximumIntake;
+        response.amountRatioPerMinimumIntake = amount / minimumIntake;
+        response.maximumIntakeRatioPerMinimumIntake = maximumIntake / minimumIntake;
 
         return response;
     }
