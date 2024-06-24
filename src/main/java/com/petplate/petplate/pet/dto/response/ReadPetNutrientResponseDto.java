43a2ef56a6fc2479c3155bdb3895faac03326e2f
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-06-24 17:06:07 +0900

    Refactor: 매개변수 이름 변경 (#24)
    weight -> amount

--- src/main/java/com/petplate/petplate/pet/dto/response/ReadPetNutrientResponseDto.java
+++ src/main/java/com/petplate/petplate/pet/dto/response/ReadPetNutrientResponseDto.java
@@ -1,7 +1,5 @@
 package com.petplate.petplate.pet.dto.response;
 
-import com.petplate.petplate.common.EmbeddedType.Nutrient;
-import com.petplate.petplate.petdailymeal.domain.entity.DailyMeal;
 import lombok.Getter;
 
 @Getter
@@ -9,14 +7,14 @@ public class ReadPetNutrientResponseDto {
     private String name;
     private String unit;
     private String description;
-    private double weight;
+    private double amount;
 
-    public static ReadPetNutrientResponseDto from(String name, String unit, String description,double weight) {
+    public static ReadPetNutrientResponseDto from(String name, String unit, String description, double amount) {
         ReadPetNutrientResponseDto response = new ReadPetNutrientResponseDto();
         response.name = name;
         response.unit = unit;
         response.description = description;
-        response.weight = weight;
+        response.amount = amount;
 
         return response;
     }
