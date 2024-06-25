2f124174a1dcd67265997b9fd4bf2ccab283c81e
Jinu Yi <dionisos198@naver.com>
2024-06-26 04:20:20 +0900

    Feat: jwt 인증 및 인가 handler,filter (#21)

---
+++ src/main/java/com/petplate/petplate/auth/jwt/handler/JwtAccessDeniedHandler.java
@@ -0,0 +1,29 @@
+package com.petplate.petplate.auth.jwt.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.petplate.petplate.common.response.BaseResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static final String NOT_AUTHORIZED="권한이 존재하지 않습니다";
+    private final ObjectMapper objectMapper;
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(BaseResponse.createError(NOT_AUTHORIZED)));
+    }
+}
