dd73ad37889e5ce1b06ff13c6866a7883ae45435
Min Jeon <mulsoap0504@gmail.com>
2024-06-27 23:27:04 +0900

    Refactor: 활동량을 네 단계로 수정. 칼로리 계산 삭제 (#33)
    - 활동량 구분을 기존 세단계에서 네단계로 수정
    - 섭취 칼로리 계산 로직을 제거

--- src/main/java/com/petplate/petplate/pet/domain/Activity.java
+++ src/main/java/com/petplate/petplate/pet/domain/Activity.java
@@ -1,23 +1,18 @@
 package com.petplate.petplate.pet.domain;
 
+import lombok.Getter;
+
 public enum Activity {
-    ACTIVE(125),
-    NORMAL(110),
-    CALM(90);
+    INACTIVE(1.0),
+    SOMEWHAT_ACTIVE(1.2),
+    ACTIVE(1.4),
+    VERY_ACTIVE(1.6);
 
-    private double properKcalPerWeight;
 
-    Activity(double properKcalPerWeight) {
-        this.properKcalPerWeight = properKcalPerWeight;
-    }
+    @Getter
+    private double value;
 
-    // 적정 섭취 칼로리 반환
-    public double getProperKcal(double weight) {
-        return this.properKcalPerWeight * weight;
-    }
-
-    // 적정 섭취 칼로리 대비 실제 섭취 칼로리에 대한 비율 반환
-    public double getProperKcalRatio(double weight, double kcal) {
-        return kcal / this.properKcalPerWeight * weight;
+    Activity(double value) {
+        this.value = value;
     }
 }
