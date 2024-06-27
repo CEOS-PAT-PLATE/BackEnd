48df7f3a2c4021e7417fad87fc5d2104a22e19c5
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-06-28 01:16:39 +0900

    Refactor: request시 반려견 pk도 같이 포함되도록 수정

--- src/main/java/com/petplate/petplate/pet/dto/request/ModifyPetInfoRequestDto.java
+++ src/main/java/com/petplate/petplate/pet/dto/request/ModifyPetInfoRequestDto.java
@@ -1,13 +1,23 @@
 package com.petplate.petplate.pet.dto.request;
-
 import com.petplate.petplate.pet.domain.Activity;
 import com.petplate.petplate.pet.domain.Neutering;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class ModifyPetInfoRequestDto {
+    @NotNull(message = "반려견이 선택되지 않았습니다")
+    private Long petId;
+
     private String name;
     private Integer age;
     private Double weight;
