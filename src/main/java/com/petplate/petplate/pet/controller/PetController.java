aa516167626cf4fc5237c70460179251147c8585
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-06-29 22:03:53 +0900

    Feat: controller 생성 (#39)

---
+++ src/main/java/com/petplate/petplate/pet/controller/PetController.java
@@ -0,0 +1,10 @@
+package com.petplate.petplate.pet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PetController {
+}
