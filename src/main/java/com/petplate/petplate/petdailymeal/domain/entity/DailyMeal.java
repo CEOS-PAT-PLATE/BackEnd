5b736010cdbf7ff34e351fc9324c55a6e8b54465
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-07-01 22:15:58 +0900

    Feat:  칼로리 / 영양소 추가, 제거 로직 (#46)

--- src/main/java/com/petplate/petplate/petdailymeal/domain/entity/DailyMeal.java
+++ src/main/java/com/petplate/petplate/petdailymeal/domain/entity/DailyMeal.java
@@ -1,6 +1,8 @@
 package com.petplate.petplate.petdailymeal.domain.entity;
 
 import com.petplate.petplate.common.EmbeddedType.Nutrient;
+import com.petplate.petplate.common.EmbeddedType.Vitamin;
+import com.petplate.petplate.common.Inheritance.BaseEntity;
 import com.petplate.petplate.pet.domain.entity.Pet;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
@@ -15,14 +17,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-
-import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class DailyMeal {
+public class DailyMeal extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,14 +29,10 @@ public class DailyMeal {
     private Long id;
 
     @Column(nullable = false)
-    private double kcal;
-
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "TIMESTAMP")
-    private LocalDate createdAt;
+    private double kcal;  // 하루 섭취 총 칼로리
 
     @Embedded
-    private Nutrient dailyNutrient;
+    private Nutrient nutrient;  // 하루 섭취 총 영양소
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "pet_id", nullable = false)
@@ -45,10 +40,51 @@ public class DailyMeal {
 
     // 이미 펫이 해당 날짜에 DailyMeal이 존재하는 경우 생성되면 안됨.(서비스에서 해결)
     @Builder
-    public DailyMeal(Nutrient dailyNutrient, Pet pet, double kcal) {
+    public DailyMeal(Nutrient nutrient, Pet pet, double kcal) {
         this.kcal = kcal;
-        this.dailyNutrient = dailyNutrient;
+        this.nutrient = nutrient;
         this.pet = pet;
-        createdAt = LocalDate.now();
+    }
+
+    // 기존 섭취하였었던 영양소에 추가된 영양소를 더해줌
+    public void addNutrient(Nutrient nutrient) {
+        this.nutrient = Nutrient.builder()
+                .carbonHydrate(this.nutrient.getCarbonHydrate() + nutrient.getCarbonHydrate())
+                .protein(this.nutrient.getProtein() + nutrient.getProtein())
+                .fat(this.nutrient.getFat() + nutrient.getFat())
+                .calcium(this.nutrient.getCalcium() + nutrient.getCalcium())
+                .phosphorus(this.nutrient.getPhosphorus() + nutrient.getPhosphorus())
+                .vitamin(Vitamin.builder()
+                        .vitaminA(this.nutrient.getVitamin().getVitaminA() + nutrient.getVitamin().getVitaminA())
+                        .vitaminD(this.nutrient.getVitamin().getVitaminD() + nutrient.getVitamin().getVitaminD())
+                        .vitaminE(this.nutrient.getVitamin().getVitaminE() + nutrient.getVitamin().getVitaminE())
+                        .build())
+                .build();
+    }
+
+    // 기존 섭취하였었던 kcal에 추가된 kcal을 더해줌
+    public void addKcal(double kcal) {
+        this.kcal = this.kcal + kcal;
+    }
+
+    // 기존 섭취하였었던 영양소에서 제거된 식사내역의 영양소를 빼줌
+    public void subtractNutrient(Nutrient nutrient) {
+        this.nutrient = Nutrient.builder()
+                .carbonHydrate(this.nutrient.getCarbonHydrate() - nutrient.getCarbonHydrate())
+                .protein(this.nutrient.getProtein() - nutrient.getProtein())
+                .fat(this.nutrient.getFat() - nutrient.getFat())
+                .calcium(this.nutrient.getCalcium() - nutrient.getCalcium())
+                .phosphorus(this.nutrient.getPhosphorus() - nutrient.getPhosphorus())
+                .vitamin(Vitamin.builder()
+                        .vitaminA(this.nutrient.getVitamin().getVitaminA() - nutrient.getVitamin().getVitaminA())
+                        .vitaminD(this.nutrient.getVitamin().getVitaminD() - nutrient.getVitamin().getVitaminD())
+                        .vitaminE(this.nutrient.getVitamin().getVitaminE() - nutrient.getVitamin().getVitaminE())
+                        .build())
+                .build();
+    }
+
+    // 기존 섭취하였었던 kcal에 제거된 식사내역의 kcal을 빼줌
+    public void subtractKcal(double kcal) {
+        this.kcal = this.kcal - kcal;
     }
 }
