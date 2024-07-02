4a96ac7312da154b224708eb16a9152f807b3a4f
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-07-02 18:25:05 +0900

    Feat: CreatedAt으로 정렬하여 조회하는 로직 추가 (#46)

--- src/main/java/com/petplate/petplate/petdailymeal/repository/DailyMealRepository.java
+++ src/main/java/com/petplate/petplate/petdailymeal/repository/DailyMealRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface DailyMealRepository extends JpaRepository<DailyMeal, Long> {
     Optional<DailyMeal> findByPetIdAndCreatedAtBetween(Long petId, LocalDateTime start, LocalDateTime end);
     boolean existsByPetIdAndCreatedAtBetween(Long petId, LocalDateTime start, LocalDateTime end);
+    List<DailyMeal> findByPetIdOrderByCreatedAtDesc(Long petId);
 }
