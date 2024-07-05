f9f65b5f433b8bffd405e4bb83ce662758fa6909
Jinu Yi <dionisos198@naver.com>
2024-07-05 16:24:41 +0900

    Feat: User 가 자신의 프로필 조회하는 기능 (#60)

--- src/main/java/com/petplate/petplate/user/service/UserService.java
+++ src/main/java/com/petplate/petplate/user/service/UserService.java
@@ -1,5 +1,9 @@
 package com.petplate.petplate.user.service;
 
+import com.petplate.petplate.common.response.error.ErrorCode;
+import com.petplate.petplate.common.response.error.exception.NotFoundException;
+import com.petplate.petplate.user.domain.entity.User;
+import com.petplate.petplate.user.dto.response.MyProfileResponseDto;
 import com.petplate.petplate.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public MyProfileResponseDto showMyProfile(String username){
+
+        User findUser = findUserByUsername(username);
+
+        return MyProfileResponseDto.from(findUser);
+
+    }
+
+    private User findUserByUsername(String username){
+        return userRepository.findByUsername(username).orElseThrow(()->new NotFoundException(
+                ErrorCode.USER_NOT_FOUND));
+    }
 
 
 }
