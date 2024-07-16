254f51437f89fe51a7495a8d9c8afdaa2701d346
Min Jeon <mulsoap0504@gmail.com>
2024-07-16 13:09:21 +0900

    Refactor: @PathVariable, @RequestParam value값 명시 (#89)

--- src/main/java/com/petplate/petplate/petfood/repository/RawRepository.java
+++ src/main/java/com/petplate/petplate/petfood/repository/RawRepository.java
@@ -3,6 +3,7 @@ package com.petplate.petplate.petfood.repository;
 import com.petplate.petplate.petfood.domain.entity.Raw;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +11,7 @@ import java.util.Optional;
 public interface RawRepository extends JpaRepository<Raw, Long> {
 
     @Query("select r from Raw r where r.name like %:keyword%")
-    List<Raw> findByKeyword(String keyword);
+    List<Raw> findByKeyword(@Param("keyword") String keyword);
 
     Optional<Raw> findByName(String name);
 
