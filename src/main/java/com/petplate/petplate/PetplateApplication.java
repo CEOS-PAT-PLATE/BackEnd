2680dd561164024597ccc0f0578b25eddeb2a86c
Jinu Yi <dionisos198@naver.com>
2024-07-18 03:43:54 +0900

    Refactor: time zone 변경

--- src/main/java/com/petplate/petplate/PetplateApplication.java
+++ src/main/java/com/petplate/petplate/PetplateApplication.java
@@ -1,5 +1,7 @@
 package com.petplate.petplate;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +12,12 @@ public class PetplateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetplateApplication.class, args);
+	}
+
+
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
