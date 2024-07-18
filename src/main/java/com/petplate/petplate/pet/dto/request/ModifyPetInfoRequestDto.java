02c4ff515d4a3737d05780b83fe37ae0a0432a8e
riceCakeSsamanKo <mulsoap0504@gmail.com>
2024-07-18 20:16:15 +0900

    Refactor: 반려견 최소 몸무게 50g으로 설정 (#115)

--- src/main/java/com/petplate/petplate/pet/dto/request/ModifyPetInfoRequestDto.java
+++ src/main/java/com/petplate/petplate/pet/dto/request/ModifyPetInfoRequestDto.java
@@ -1,10 +1,7 @@
 package com.petplate.petplate.pet.dto.request;
 import com.petplate.petplate.pet.domain.Activity;
 import com.petplate.petplate.pet.domain.Neutering;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,7 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ModifyPetInfoRequestDto {
     private String name;
+    @Min(value = 0, message = "잘못된 나이 입력입니다. (나이는 최소 1살부터 최대 40살까지입니다.)")
+    @Max(value = 41, message = "잘못된 나이 입력입니다. (나이는 최소 1살부터 최대 40살까지입니다.)")
     private Integer age;
+    @DecimalMin(value = "0.05", message = "잘못된 체중 입력입니다. (체중은 50g 초과 100kg 미만입니다.)")
+    @Max(value = 100, message = "잘못된 체중 입력입니다. (체중은 0kg 초과 100kg 미만입니다.)")
     private Double weight;
     private Activity activity;
     private Neutering neutering;
