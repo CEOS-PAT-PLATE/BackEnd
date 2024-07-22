c428929a798ccb08fbc5e8697657865ba87f4e0b
Jinu Yi <dionisos198@naver.com>
2024-07-22 13:11:50 +0900

    Feat: Cors 에러 수정 (#126)

--- src/main/java/com/petplate/petplate/common/config/WebConfig.java
+++ src/main/java/com/petplate/petplate/common/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
                 .allowedOrigins(
-                        "http://localhost:3000") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
+                        "http://localhost:3000","http://petplate.kr","https://petplate.kr") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
                 .allowedMethods("GET", "POST", "PUT", "PATCH",
                         "DELETE", "OPTIONS") // cors를 허용할 method + DELETE 추가
                 .allowedHeaders("Content-Type", "Authorization","accessToken")
